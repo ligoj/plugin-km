56b97d90837a3774c9e919a412cdad901f9168b9
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-04-03 09:00:15 +0200

    Initialization

--- src/test/java/org/ligoj/app/plugin/km/KmResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/km/KmResourceTest.java
@@ -1,13 +1,16 @@
 package org.ligoj.app.plugin.km;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ligoj.bootstrap.AbstractJpaTest;
+import org.ligoj.app.AbstractAppTest;
+import org.ligoj.app.model.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,10 +23,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
 @Rollback
 @Transactional
-public class KmResourceTest extends AbstractJpaTest {
+public class KmResourceTest extends AbstractAppTest {
 
 	@Autowired
 	private KmResource resource;
+
+	@Before
+	public void prepareData() throws IOException {
+		persistEntities("csv", new Class[] { Node.class }, StandardCharsets.UTF_8.name());
+	}
 
 	@Test
 	public void getKey() throws IOException {
